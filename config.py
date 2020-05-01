2aca31a39656843466ca913cb4f076137872cab3
Jerry <xzhou@scripps.edu>
2020-04-30 19:21:10 -0700

    Update API access to include covid-zenodo

---
+++ config.py
@@ -0,0 +1,42 @@
+# *****************************************************************************
+# Elasticsearch variables
+# *****************************************************************************
+ES_HOST = 'localhost:9200'
+ES_INDEX = 'zenodo_covid'
+ES_DOC_TYPE = 'zenodo'
+ES_INDICES = {
+    "covid19":"outbreak-covid19",
+    "zenodo":"zenodo_covid"
+}
+
+# *****************************************************************************
+# Web Application
+# *****************************************************************************
+
+API_PREFIX = 'resources'
+API_VERSION = ''
+
+# *****************************************************************************
+# Analytical Statistics
+# *****************************************************************************
+
+GA_ACTION_QUERY_GET = 'query_get'
+GA_ACTION_QUERY_POST = 'query_post'
+GA_ACTION_ANNOTATION_GET = 'outbreak_get'
+GA_ACTION_ANNOTATION_POST = 'outbreak_post'
+GA_TRACKER_URL = 'api.outbreak.info'
+
+# *****************************************************************************
+# Endpoint Specifics
+# *****************************************************************************
+
+STATUS_CHECK = {
+    'id': 'USA_US-CA_06073_2020-03-01',  # San Diego County
+    'index': 'outbreak-covid19',
+    'doc_type': 'outbreak_info'
+}
+
+try:
+    from config_web_local import *
+except ImportError:
+    pass
