400fbea440910f93dafa322973f7c3785a9ae7b3
Jerry <xzhou@scripps.edu>
2020-06-04 20:52:44 -0700

    Move zenodo transformation to index stage

--- web/pipeline.py
+++ web/pipeline.py
@@ -33,19 +33,3 @@ class ResourceTransform(ESResultTransform):
     """
     TODO
     """
-
-    def transform_hit(self, path, doc, options):
-
-        if path == '':
-            _index = doc.pop('_index')
-            doc.pop('_type', None)    # not available by default on es7
-            doc.pop('sort', None)     # added when using sort
-            doc.pop('_node', None)    # added when using explain
-            doc.pop('_shard', None)   # added when using explain
-            if 'zenodo' in _index:
-                # simplify id
-                doc['_id'] = 'zenodo.' + doc.pop('_id').split('.')[-1]
-                # fix fake list
-                if 'keywords' in doc and len(doc['keywords']) == 1:
-                    doc['keywords'] = re.split(r', |,|; |;', doc['keywords'][0])
-            # add more source customizations here
