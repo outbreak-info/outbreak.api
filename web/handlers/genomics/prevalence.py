f3f6cb45e246884f10428483ce42d5748f71ae57
gkarthik <xraydiffraction@gmail.com>
2021-03-01 14:49:50 -0800

    Merged country,division and county into one handler

--- web/handlers/genomics/prevalence.py
+++ web/handlers/genomics/prevalence.py
@@ -47,24 +47,26 @@ class PrevalenceByLocationHandler(BaseHandler):
         self.write(resp)
 
 # Most recent prevalence
-class PrevalenceByCountryAndTimeHandler(BaseHandler):
+class PrevalenceByLocationAndTimeHandler(BaseHandler):
 
     @gen.coroutine
     def get(self):
         query_pangolin_lineage = self.get_argument("pangolin_lineage", None)
         query_detected = self.get_argument("detected", None)
         query_mutations = self.get_argument("mutations", None)
+        query_country = self.get_argument("country", None)
+        query_division = self.get_argument("division", None)
         query_mutations = query_mutations.split(",") if query_mutations is not None else []
         query_detected = True if query_detected == "true" else False
         query = {
             "size": 0,
             "aggs": {
-                "country_date_buckets": {
+                "sub_date_buckets": {
                     "composite": {
                         "size": 10000,
                         "sources": [
                             {"date_collected": { "terms": {"field": "date_collected"}}},
-                            {"country": { "terms": {"field": "country"} }}
+                            # {"country": { "terms": {"field": "country"} }}
                         ]
                     },
                     "aggregations": {
@@ -75,131 +77,43 @@ class PrevalenceByCountryAndTimeHandler(BaseHandler):
                 }
             }
         }
+        if query_division is not None:
+            query["query"] = {
+	        "term": {"division": query_division}
+            }
+            query["aggs"]["sub_date_buckets"]["composite"]["sources"].append(
+                {"sub": { "terms": {"field": "location"} }}
+            )
+        elif query_country is not None:
+            query["query"] = {
+	        "term": {"country": query_country}
+            }
+            query["aggs"]["sub_date_buckets"]["composite"]["sources"].append(
+                {"sub": { "terms": {"field": "division"} }}
+            )
+        else:
+            query["aggs"]["sub_date_buckets"]["composite"]["sources"].append(
+                {"sub": { "terms": {"field": "country"} }}
+            )
         query_obj = create_nested_mutation_query(lineage = query_pangolin_lineage, mutations = query_mutations)
-        query["aggs"]["country_date_buckets"]["aggregations"]["lineage_count"]["filter"] = query_obj
+        query["aggs"]["sub_date_buckets"]["aggregations"]["lineage_count"]["filter"] = query_obj
         resp = yield self.asynchronous_fetch(query)
         ctr = 0
-        buckets = resp["aggregations"]["country_date_buckets"]["buckets"]
+        buckets = resp["aggregations"]["sub_date_buckets"]["buckets"]
         # Get all paginated results
-        while "after_key" in resp["aggregations"]["country_date_buckets"]:
-            query["aggs"]["country_date_buckets"]["composite"]["after"] = resp["aggregations"]["country_date_buckets"]["after_key"]
+        while "after_key" in resp["aggregations"]["sub_date_buckets"]:
+            query["aggs"]["sub_date_buckets"]["composite"]["after"] = resp["aggregations"]["sub_date_buckets"]["after_key"]
             resp = yield self.asynchronous_fetch(query)
-            buckets.extend(resp["aggregations"]["country_date_buckets"]["buckets"])
+            buckets.extend(resp["aggregations"]["sub_date_buckets"]["buckets"])
         dict_response = {"success": True, "results": []}
         if len(buckets) > 0:
             flattened_response = [{
                 "date": i["key"]["date_collected"],
-                "name": i["key"]["country"],
+                "name": i["key"]["sub"],
                 "total_count": i["doc_count"],
                 "lineage_count": i["lineage_count"]["doc_count"]
             } for i in buckets if len(i["key"]["date_collected"].split("-")) > 1 and "XX" not in i["key"]["date_collected"]]
             dict_response = transform_prevalence_by_location_and_tiime(flattened_response, query_detected)
-        resp = {"success": True, "results": dict_response}
-        self.write(resp)
-
-class PrevalenceByDivisionAndTimeHandler(BaseHandler):
-
-    @gen.coroutine
-    def get(self):
-        query_pangolin_lineage = self.get_argument("pangolin_lineage", None)
-        query_country = self.get_argument("country", None)
-        query_detected = self.get_argument("detected", None)
-        query_mutations = self.get_argument("mutations", None)
-        query_mutations = query_mutations.split(",") if query_mutations is not None else []
-        query_detected = True if query_detected == "true" else False
-        query =      {
-            "size": 0,
-            "query": {
-	        "term": {"country": query_country}
-            },
-            "aggs": {
-                "division_date_buckets": {
-                    "composite": {
-                        "size": 10000,
-                        "sources": [
-                            {"date_collected": { "terms": {"field": "date_collected"}}},
-                            {"division": { "terms": {"field": "division"} }}
-                        ]
-                    },
-                    "aggregations": {
-                        "lineage_count": {
-                            "filter": {}
-                        }
-                    }
-                }
-            }
-        }
-        query_obj = create_nested_mutation_query(lineage = query_pangolin_lineage, mutations = query_mutations)
-        query["aggs"]["division_date_buckets"]["aggregations"]["lineage_count"]["filter"] = query_obj
-        resp = yield self.asynchronous_fetch(query)
-        buckets = resp["aggregations"]["division_date_buckets"]["buckets"]
-        # Get all paginated results
-        while "after_key" in resp["aggregations"]["division_date_buckets"]:
-            query["aggs"]["division_date_buckets"]["composite"]["after"] = resp["aggregations"]["division_date_buckets"]["after_key"]
-            resp = yield self.asynchronous_fetch(query)
-            buckets.extend(resp["aggregations"]["division_date_buckets"]["buckets"])
-        if len(buckets) == 0:
-            return {"success": True, "results": []}
-        flattened_response = [{
-            "date": i["key"]["date_collected"],
-            "name": i["key"]["division"],
-            "total_count": i["doc_count"],
-            "lineage_count": i["lineage_count"]["doc_count"]
-        } for i in buckets if len(i["key"]["date_collected"].split("-")) > 1 and "XX" not in i["key"]["date_collected"]]
-        dict_response = transform_prevalence_by_location_and_tiime(flattened_response, query_detected)
-        resp = {"success": True, "results": dict_response}
-        self.write(resp)
-
-class PrevalenceByCountyAndTimeHandler(BaseHandler):
-
-    @gen.coroutine
-    def get(self):
-        query_pangolin_lineage = self.get_argument("pangolin_lineage", None)
-        query_division = self.get_argument("division", None)
-        query_detected = self.get_argument("detected", None)
-        query_mutations = self.get_argument("mutations", None)
-        query_mutations = query_mutations.split(",") if query_mutations is not None else []
-        query_detected = True if query_detected == "true" else False
-        query =      {
-            "size": 0,
-            "query": {
-	        "term": {"division": query_division}
-            },
-            "aggs": {
-                "division_date_buckets": {
-                    "composite": {
-                        "size": 10000,
-                        "sources": [
-                            {"date_collected": { "terms": {"field": "date_collected"}}},
-                            {"division": { "terms": {"field": "location"} }}
-                        ]
-                    },
-                    "aggregations": {
-                        "lineage_count": {
-                            "filter": {}
-                        }
-                    }
-                }
-            }
-        }
-        query_obj = create_nested_mutation_query(lineage = query_pangolin_lineage, mutations = query_mutations)
-        query["aggs"]["division_date_buckets"]["aggregations"]["lineage_count"]["filter"] = query_obj
-        resp = yield self.asynchronous_fetch(query)
-        buckets = resp["aggregations"]["division_date_buckets"]["buckets"]
-        # Get all paginated results
-        while "after_key" in resp["aggregations"]["division_date_buckets"]:
-            query["aggs"]["division_date_buckets"]["composite"]["after"] = resp["aggregations"]["division_date_buckets"]["after_key"]
-            resp = yield self.asynchronous_fetch(query)
-            buckets.extend(resp["aggregations"]["division_date_buckets"]["buckets"])
-        if len(buckets) == 0:
-            return {"success": True, "results": []}
-        flattened_response = [{
-            "date": i["key"]["date_collected"],
-            "name": i["key"]["division"],
-            "total_count": i["doc_count"],
-            "lineage_count": i["lineage_count"]["doc_count"]
-        } for i in buckets if len(i["key"]["date_collected"].split("-")) > 1 and "XX" not in i["key"]["date_collected"]]
-        dict_response = transform_prevalence_by_location_and_tiime(flattened_response, query_detected)
         resp = {"success": True, "results": dict_response}
         self.write(resp)
 
