4b9301330ec10afc2155fe7af15f89b2421717a9
gkarthik <xraydiffraction@gmail.com>
2021-03-08 21:19:58 -0800

    Switched to composite aggregation. @gkarthik is even stupider than expected :facepalm:

--- web/handlers/genomics/general.py
+++ web/handlers/genomics/general.py
@@ -223,182 +223,80 @@ class LocationHandler(BaseHandler):
     # Use dict to map to NE IDs from epi data
     country_iso3_to_iso2 = {"BGD": "BD", "BEL": "BE", "BFA": "BF", "BGR": "BG", "BIH": "BA", "BRB": "BB", "WLF": "WF", "BLM": "BL", "BMU": "BM", "BRN": "BN", "BOL": "BO", "BHR": "BH", "BDI": "BI", "BEN": "BJ", "BTN": "BT", "JAM": "JM", "BVT": "BV", "BWA": "BW", "WSM": "WS", "BES": "BQ", "BRA": "BR", "BHS": "BS", "JEY": "JE", "BLR": "BY", "BLZ": "BZ", "RUS": "RU", "RWA": "RW", "SRB": "RS", "TLS": "TL", "REU": "RE", "TKM": "TM", "TJK": "TJ", "ROU": "RO", "TKL": "TK", "GNB": "GW", "GUM": "GU", "GTM": "GT", "SGS": "GS", "GRC": "GR", "GNQ": "GQ", "GLP": "GP", "JPN": "JP", "GUY": "GY", "GGY": "GG", "GUF": "GF", "GEO": "GE", "GRD": "GD", "GBR": "GB", "GAB": "GA", "SLV": "SV", "GIN": "GN", "GMB": "GM", "GRL": "GL", "GIB": "GI", "GHA": "GH", "OMN": "OM", "TUN": "TN", "JOR": "JO", "HRV": "HR", "HTI": "HT", "HUN": "HU", "HKG": "HK", "HND": "HN", "HMD": "HM", "VEN": "VE", "PRI": "PR", "PSE": "PS", "PLW": "PW", "PRT": "PT", "SJM": "SJ", "PRY": "PY", "IRQ": "IQ", "PAN": "PA", "PYF": "PF", "PNG": "PG", "PER": "PE", "PAK": "PK", "PHL": "PH", "PCN": "PN", "POL": "PL", "SPM": "PM", "ZMB": "ZM", "ESH": "EH", "EST": "EE", "EGY": "EG", "ZAF": "ZA", "ECU": "EC", "ITA": "IT", "VNM": "VN", "SLB": "SB", "ETH": "ET", "SOM": "SO", "ZWE": "ZW", "SAU": "SA", "ESP": "ES", "ERI": "ER", "MNE": "ME", "MDA": "MD", "MDG": "MG", "MAF": "MF", "MAR": "MA", "MCO": "MC", "UZB": "UZ", "MMR": "MM", "MLI": "ML", "MAC": "MO", "MNG": "MN", "MHL": "MH", "MKD": "MK", "MUS": "MU", "MLT": "MT", "MWI": "MW", "MDV": "MV", "MTQ": "MQ", "MNP": "MP", "MSR": "MS", "MRT": "MR", "IMN": "IM", "UGA": "UG", "TZA": "TZ", "MYS": "MY", "MEX": "MX", "ISR": "IL", "FRA": "FR", "IOT": "IO", "SHN": "SH", "FIN": "FI", "FJI": "FJ", "FLK": "FK", "FSM": "FM", "FRO": "FO", "NIC": "NI", "NLD": "NL", "NOR": "NO", "NAM": "NA", "VUT": "VU", "NCL": "NC", "NER": "NE", "NFK": "NF", "NGA": "NG", "NZL": "NZ", "NPL": "NP", "NRU": "NR", "NIU": "NU", "COK": "CK", "XKX": "XK", "CIV": "CI", "CHE": "CH", "COL": "CO", "CHN": "CN", "CMR": "CM", "CHL": "CL", "CCK": "CC", "CAN": "CA", "COG": "CG", "CAF": "CF", "COD": "CD", "CZE": "CZ", "CYP": "CY", "CXR": "CX", "CRI": "CR", "CUW": "CW", "CPV": "CV", "CUB": "CU", "SWZ": "SZ", "SYR": "SY", "SXM": "SX", "KGZ": "KG", "KEN": "KE", "SSD": "SS", "SUR": "SR", "KIR": "KI", "KHM": "KH", "KNA": "KN", "COM": "KM", "STP": "ST", "SVK": "SK", "KOR": "KR", "SVN": "SI", "PRK": "KP", "KWT": "KW", "SEN": "SN", "SMR": "SM", "SLE": "SL", "SYC": "SC", "KAZ": "KZ", "CYM": "KY", "SGP": "SG", "SWE": "SE", "SDN": "SD", "DOM": "DO", "DMA": "DM", "DJI": "DJ", "DNK": "DK", "VGB": "VG", "DEU": "DE", "YEM": "YE", "DZA": "DZ", "USA": "US", "URY": "UY", "MYT": "YT", "UMI": "UM", "LBN": "LB", "LCA": "LC", "LAO": "LA", "TUV": "TV", "TWN": "TW", "TTO": "TT", "TUR": "TR", "LKA": "LK", "LIE": "LI", "LVA": "LV", "TON": "TO", "LTU": "LT", "LUX": "LU", "LBR": "LR", "LSO": "LS", "THA": "TH", "ATF": "TF", "TGO": "TG", "TCD": "TD", "TCA": "TC", "LBY": "LY", "VAT": "VA", "VCT": "VC", "ARE": "AE", "AND": "AD", "ATG": "AG", "AFG": "AF", "AIA": "AI", "VIR": "VI", "ISL": "IS", "IRN": "IR", "ARM": "AM", "ALB": "AL", "AGO": "AO", "ATA": "AQ", "ASM": "AS", "ARG": "AR", "AUS": "AU", "AUT": "AT", "ABW": "AW", "IND": "IN", "ALA": "AX", "AZE": "AZ", "IRL": "IE", "IDN": "ID", "UKR": "UA", "QAT": "QA", "MOZ": "MZ"}
 
+    location_types = ["country", "division", "location"]
+
     @gen.coroutine
     def get(self):
         query_str = self.get_argument("name", None)
-        query = {
-            "size": 0,
-            "aggs": {
-                "country_agg": {
-                    "filter": {
-                        "wildcard": {
-                            "country_lower": {
-                                "value": query_str
-                            }
-                        }
-                    },
-                    "aggs": {
-                        "country_sub_agg": {
-                            "terms": {
-                                "field": "country"
-                            },
-                            "aggs": {
-                                "country_id": {
-                                    "terms": {
-                                        "field": "country_id"
-                                    }
-                                }
-                            }
+        flattened_response = []
+        for loc in self.location_types:
+            query = {
+                "size": 0,
+                "query": {
+                    "wildcard": {
+                        "{}_lower".format(loc): {
+                            "value": query_str
                         }
                     }
                 },
-                "division_agg": {
-                    "filter": {
-                        "wildcard": {
-                            "division_lower": {
-                                "value": query_str
-                            }
-                        }
-                    },
-                    "aggs": {
-                        "division_sub_agg": {
-                            "terms": {
-                                "field": "division"
-                            },
-                            "aggs": {
-                                "country": {
-                                    "terms": {
-                                        "field": "country"
-                                    },
-                                    "aggs": {
-                                        "country_id": {
-                                            "terms": {
-                                                "field": "country_id"
-                                            },
-                                            "aggs": {
-                                                "division_id": {
-                                                    "terms": {
-                                                        "field": "division_id"
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
-                "location_agg": {
-                    "filter": {
-                        "wildcard": {
-                            "location_lower": {
-                                "value": query_str
-                            }
-                        }
-                    },
-                    "aggs": {
-                        "location_sub_agg": {
-                            "terms": {
-                                "field": "location"
-                            },
-                            "aggs": {
-                                "country": {
-                                    "terms": {
-                                        "field": "country"
-                                    },
-                                    "aggs": {
-                                        "country_id": {
-                                            "terms": {
-                                                "field": "country_id"
-                                            },
-                                            "aggs": {
-                                                "division": {
-                                                    "terms": {
-                                                        "field": "division"
-                                                    },
-                                                    "aggs": {
-                                                        "division_id": {
-                                                            "terms": {
-                                                                "field": "division_id"
-                                                            },
-                                                            "aggs": {
-                                                                "location_id": {
-                                                                    "terms": {
-                                                                        "field": "location_id"
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                "aggs": {
+                    "loc_agg": {
+                        "composite": {
+                            "size": 10000,
+                            "sources": [
+                                {loc: { "terms": {"field": loc}}},
+                                {"{}_id".format(loc): { "terms": {"field": "{}_id".format(loc)} }}
+                            ]
                         }
                     }
                 }
             }
-        }
-        resp = yield self.asynchronous_fetch(query)
-        # Parse countries
-        flattened_response = []
-        for rec in resp["aggregations"]["country_agg"]["country_sub_agg"]["buckets"]:
-            for rec_id in rec["country_id"]["buckets"][:1]:
-                flattened_response.append({
-                    "country": rec["key"],
-                    "country_id": rec_id["key"], # There should be only 1 ID associated with each country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                    "total_count": rec_id["doc_count"] # Should be same as rec["doc_count"] siince only 1 ID per location
-                })
-        # Parse divisions
-        for rec in resp["aggregations"]["division_agg"]["division_sub_agg"]["buckets"]:
-            for rec_country in rec["country"]["buckets"]: # Multiple countries per division name
-                for rec_country_id in rec_country["country_id"]["buckets"][:1]: # There should be only 1 ID associated with each country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                    for rec_division_id in rec_country_id["division_id"]["buckets"][:1]: # There should be only 1 ID associated with each division within a country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                        flattened_response.append({
-                            "division": rec["key"],
-                            "division_id": rec_division_id["key"],
-                            "country": rec_country["key"],
-                            "country_id": rec_country_id["key"], # There should be only 1 ID associated with each country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                            "total_count": rec_division_id["doc_count"] # Should be same as rec_country["doc_count"] siince only 1 ID per division
-                        })
-        # Parse counties
-        for rec in resp["aggregations"]["location_agg"]["location_sub_agg"]["buckets"]:
-            for rec_country in rec["country"]["buckets"]: # Multiple countries per county
-                for rec_country_id in rec_country["country_id"]["buckets"][:1]: # There should be only 1 ID associated with each country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                    for rec_division in rec_country_id["division"]["buckets"]: # Multiple divisions per county
-                        for rec_division_id in rec_division["division_id"]["buckets"][:1]: # There should be only 1 ID associated with each division within a country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                            for rec_location_id in rec_division_id["location_id"]["buckets"][:1]: # 1 location id per division
-                                flattened_response.append({
-                                    "location": rec["key"],
-                                    "location_id": rec_location_id["key"],
-                                    "division": rec_division["key"],
-                                    "division_id": rec_division_id["key"],
-                                    "country": rec_country["key"],
-                                    "country_id": rec_country_id["key"], # There should be only 1 ID associated with each country. Used this in lieu of composite aggregations. Composite aggregations don't work as a child aggregation.
-                                    "total_count": rec_location_id["doc_count"] # Should be same as rec_country["doc_count"] siince only 1 ID per division
-                                })
-        # Add full names and location ids
-        for rec in flattened_response:
-            _name = ""
-            _id = ""
-            admin_level = 0
-            if "location_id" in rec:
-                _name = ", ".join([rec["location"], rec["division"], rec["country"]])
-                _id = "_".join([rec["country_id"], self.country_iso3_to_iso2[rec["country_id"]] + "-" + rec["division_id"], rec["location_id"]]) # ID in the format
-                admin_level = 2
-            elif "division_id" in rec:
-                _name = ", ".join([rec["division"], rec["country"]])
-                _id = "_".join([rec["country_id"], self.country_iso3_to_iso2[rec["country_id"]] + "-" + rec["division_id"]]) # ID in the format
-                admin_level = 1
-            else:
-                _name = rec["country"]
-                _id = rec["country_id"]
-            rec["label"] = _name
-            rec["id"] = _id
-            rec["admin_level"] = admin_level
+            if loc == "division":
+                query["aggs"]["loc_agg"]["composite"]["sources"].extend([
+                    {"country": { "terms": {"field": "country"}}},
+                    {"country_id": { "terms": {"field": "country_id"}}}
+                ])
+            if loc == "location":
+                query["aggs"]["loc_agg"]["composite"]["sources"].extend([
+                    {"country": { "terms": {"field": "country"}}},
+                    {"country_id": { "terms": {"field": "country_id"}}},
+                    {"division": { "terms": {"field": "division"}}},
+                    {"division_id": { "terms": {"field": "division_id"}}}
+                ])
+            resp = yield self.asynchronous_fetch(query)
+            if loc =="country":
+                for rec in resp["aggregations"]["loc_agg"]["buckets"]:
+                    flattened_response.append({
+                        "country": rec["key"]["country"],
+                        "country_id": rec["key"]["country_id"],
+                        "id": rec["key"]["country_id"],
+                        "label": rec["key"]["country"],
+                        "admin_level": 0
+                    })
+            if loc =="division":
+                for rec in resp["aggregations"]["loc_agg"]["buckets"]:
+                    flattened_response.append({
+                        "country": rec["key"]["country"],
+                        "country_id": rec["key"]["country_id"],
+                        "division": rec["key"]["division"],
+                        "division_id": rec["key"]["division_id"],
+                        "id": "_".join([rec["key"]["country_id"], self.country_iso3_to_iso2[rec["key"]["country_id"]] + "-" + rec["key"]["division_id"]]),
+                        "label": ", ".join([rec["key"]["division"], rec["key"]["country"]]),
+                        "admin_level": 1
+                    })
+            if loc =="location":
+                for rec in resp["aggregations"]["loc_agg"]["buckets"]:
+                    flattened_response.append({
+                        "country": rec["key"]["country"],
+                        "country_id": rec["key"]["country_id"],
+                        "division": rec["key"]["division"],
+                        "division_id": rec["key"]["division_id"],
+                        "location": rec["key"]["location"],
+                        "location_id": rec["key"]["location_id"],
+                        "id": "_".join([rec["key"]["country_id"], self.country_iso3_to_iso2[rec["key"]["country_id"]] + "-" + rec["key"]["division_id"], rec["key"]["location_id"]]),
+                        "label": ", ".join([rec["key"]["location"], rec["key"]["division"], rec["key"]["country"]]),
+                        "admin_level": 2
+                    })
         resp = {"success": True, "results": flattened_response}
         self.write(resp)
 
