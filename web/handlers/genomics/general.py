e948736a78e870bce20b65e9bacae13765853cfa
gkarthik <xraydiffraction@gmail.com>
2021-03-09 10:18:02 -0800

    Return ids, label and admin_level from location lookup

--- web/handlers/genomics/general.py
+++ web/handlers/genomics/general.py
@@ -115,56 +115,64 @@ class LocationDetailsHandler(BaseHandler):
         query = {
             "query": {},
             "aggs": {
-                "country": {
-                    "terms": {
-                        "field": "country"
+                "loc": {
+                    "composite": {
+                        "size": 10000,
+                        "sources": []
                     }
                 }
             }
         }
-        if len(query_ids) >= 2:
-            query["aggs"]["country"]["aggs"] = {
-                "division": {
-                    "terms": {
-                        "field": "division"
-                    }
-                }
-            }
-        if len(query_ids) == 3: # 3 is max length
-            query["aggs"]["country"]["aggs"]["division"]["aggs"] = {
-                "location": {
-                    "terms": {
-                        "field": "location"
-                    }
-                }
-            }
+        loc_id_len = len(query_ids)
+        if loc_id_len >= 1:
+            query["aggs"]["loc"]["composite"]["sources"].extend([
+                {"country": { "terms": {"field": "country"}}},
+                {"country_id": { "terms": {"field": "country_id"} }}
+            ])
+        if loc_id_len >= 2: # 3 is max length
+            query["aggs"]["loc"]["composite"]["sources"].extend([
+                {"division": { "terms": {"field": "division"}}},
+                {"division_id": { "terms": {"field": "division_id"} }}
+            ])
+        if loc_id_len == 3: # 3 is max length
+            query["aggs"]["loc"]["composite"]["sources"].extend([
+                {"location": { "terms": {"field": "location"}}},
+                {"location_id": { "terms": {"field": "location_id"} }}
+            ])
         query["query"] = parse_location_id_to_query(query_str)
         resp = yield self.asynchronous_fetch(query)
         flattened_response = []
-        for country in resp["aggregations"]["country"]["buckets"]:
-            if "division" in country:
-                for division in country["division"]["buckets"]:
-                    if "location" in division:
-                        for location in division["location"]["buckets"]:
-                            flattened_response.append({
-                                "location": location["key"],
-                                "division": division["key"],
-                                "country": country["key"],
-                                "label": ", ".join([location["key"], division["key"], country["key"]])
-                            })
-                    else:
-                        flattened_response.append({
-                            "division": division["key"],
-                            "country": country["key"],
-                            "label": ", ".join([division["key"], country["key"]])
-                        })
-            else:
+        for rec in resp["aggregations"]["loc"]["buckets"]:
+            if loc_id_len == 1:
                 flattened_response.append({
-                    "country": country["key"],
-                    "label": country["key"]
+                    "country": rec["key"]["country"],
+                    "country_id": rec["key"]["country_id"],
+                    "label": rec["key"]["country"],
+                    "admin_level": 0
+                })
+            elif loc_id_len == 2:
+                flattened_response.append({
+                    "division": rec["key"]["division"],
+                    "division_id": rec["key"]["division_id"],
+                    "country": rec["key"]["country"],
+                    "country_id": rec["key"]["country_id"],
+                    "label": ", ".join([rec["key"]["division"], rec["key"]["country"]]),
+                    "admin_level": 1
+                })
+            elif loc_id_len == 3:
+                flattened_response.append({
+                    "location": rec["key"]["location"],
+                    "location_id": rec["key"]["location_id"],
+                    "division": rec["key"]["division"],
+                    "division_id": rec["key"]["division_id"],
+                    "country": rec["key"]["country"],
+                    "country_id": rec["key"]["country_id"],
+                    "label": ", ".join([rec["key"]["location"], rec["key"]["division"], rec["key"]["country"]]),
+                    "admin_level": 2
                 })
         if len(flattened_response) >= 1:
             flattened_response = flattened_response[0] # ID should match only 1 region
+        flattened_response["query_id"] = query_str
         resp = {"success": True, "results": flattened_response}
         self.write(resp)
 
