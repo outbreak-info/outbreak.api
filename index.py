20428b72bf6c745bba14a5986c0107dccface908
remoteeng00 <remoteeng00@gmail.com>
2022-12-29 01:58:01 +0700

    Add API version 2, Add gene parameter to /lineage-mutations, Refactor handler following biothings.web format

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 from biothings.web.launcher import main
-from tornado.web import RedirectHandler
+from tornado.web import RedirectHandler, StaticFileHandler
 
 from web.handlers import ApiViewHandler, MainHandler, SpecialHandler
 from web.handlers.genomics import routes as genomics_routes
+from web.handlers.v2.genomics import routes as genomics_v2_routes
 
 if __name__ == '__main__':
     main([
         *genomics_routes,
+        *genomics_v2_routes,
         (r"/", MainHandler),
+        (r"/static/(.*)", StaticFileHandler, {"path": "./static"}),
         (r"/v1/(.*)", RedirectHandler, {"url": "/covid19/{0}"}),
         (r"/try-by-doctype/resources/?", SpecialHandler),
         (r"/try/.+", ApiViewHandler),
