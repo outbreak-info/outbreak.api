311e62ab3b320a0e474a4762faf0df35da5563c6
everaldorodrigo <everaldorodrigo@gmail.com>
2023-07-18 17:59:16 -0700

    Parse ES query result to API response.

--- web/handlers/v2/genomics/helpers/lineage_mutations_helper.py
+++ web/handlers/v2/genomics/helpers/lineage_mutations_helper.py
@@ -88,65 +88,68 @@ def create_query(lineages = "", mutations = ""):
 
 def parse_response(resp = {}, frequency = 1, genes = []):
     dict_response = {}
-    for lineage in resp["aggregations"]["lineages"]["buckets"]:
 
-        path_to_results = ["mutations", "buckets"]
+    lineage = resp["aggregations"]
+    lineage["key"] = "## TO DO"
 
-        buckets = lineage
-        for i in path_to_results:
-            buckets = buckets[i]
-        flattened_response = [
-            {
-                "mutation": i["key"],
-                "mutation_count": i["doc_count"],
-                "lineage_count": lineage["doc_count"],
-                "lineage": lineage["key"],
-            }
-            for i in buckets
-        ]
+    path_to_results = ["mutations", "buckets"]
 
-        if len(flattened_response) > 0:
-            df_response = pd.DataFrame(flattened_response).assign(
-                gene=lambda x: x["mutation"].apply(
-                    lambda k: gene_mapping()[k.split(":")[0]]
-                    if k.split(":")[0] in gene_mapping()
-                    else k.split(":")[0]
-                ),
-                ref_aa=lambda x: x["mutation"]
-                .apply(
-                    lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[0]
-                    if "DEL" not in k and "del" not in k and "_" not in k
-                    else k
-                )
-                .str.upper(),
-                alt_aa=lambda x: x["mutation"]
-                .apply(
-                    lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[1]
-                    if "DEL" not in k and "del" not in k and "_" not in k
-                    else k.split(":")[1]
-                )
-                .str.upper(),
-                codon_num=lambda x: x["mutation"].apply(
-                    lambda k: int(re.findall("[0-9]+", k.split(":")[1])[0])
-                ),
-                codon_end=lambda x: x["mutation"].apply(
-                    lambda k: int(re.findall("[0-9]+", k.split(":")[1])[1])
-                    if "/" in k and ("DEL" in k or "del" in k)
-                    else None
-                ),
-                type=lambda x: x["mutation"].apply(
-                    lambda k: "deletion" if "DEL" in k or "del" in k else "substitution"
-                ),
+    buckets = lineage
+    for i in path_to_results:
+        buckets = buckets[i]
+    flattened_response = [
+        {
+            "mutation": i["key"],
+            "mutation_count": i["doc_count"],
+            "lineage_count": lineage["hits"]["total"]["value"],
+            "lineage": lineage["key"],
+        }
+        for i in buckets
+    ]
+
+    if len(flattened_response) > 0:
+        df_response = pd.DataFrame(flattened_response).assign(
+            gene=lambda x: x["mutation"].apply(
+                lambda k: gene_mapping()[k.split(":")[0]]
+                if k.split(":")[0] in gene_mapping()
+                else k.split(":")[0]
+            ),
+            ref_aa=lambda x: x["mutation"]
+            .apply(
+                lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[0]
+                if "DEL" not in k and "del" not in k and "_" not in k
+                else k
             )
-            df_response = df_response[df_response["ref_aa"] != df_response["alt_aa"]]
-            df_response.loc[:, "prevalence"] = (
-                df_response["mutation_count"] / df_response["lineage_count"]
+            .str.upper(),
+            alt_aa=lambda x: x["mutation"]
+            .apply(
+                lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[1]
+                if "DEL" not in k and "del" not in k and "_" not in k
+                else k.split(":")[1]
             )
-            df_response.loc[~df_response["codon_end"].isna(), "change_length_nt"] = (
-                (df_response["codon_end"] - df_response["codon_num"]) + 1
-            ) * 3
-            df_response = df_response[df_response["prevalence"] >= frequency].fillna("None")
-            if genes:
-                df_response = df_response[df_response["gene"].str.lower().isin(genes)]
-            dict_response[lineage["key"]] = df_response.to_dict(orient="records")
+            .str.upper(),
+            codon_num=lambda x: x["mutation"].apply(
+                lambda k: int(re.findall("[0-9]+", k.split(":")[1])[0])
+            ),
+            codon_end=lambda x: x["mutation"].apply(
+                lambda k: int(re.findall("[0-9]+", k.split(":")[1])[1])
+                if "/" in k and ("DEL" in k or "del" in k)
+                else None
+            ),
+            type=lambda x: x["mutation"].apply(
+                lambda k: "deletion" if "DEL" in k or "del" in k else "substitution"
+            ),
+        )
+        df_response = df_response[df_response["ref_aa"] != df_response["alt_aa"]]
+        df_response.loc[:, "prevalence"] = (
+            df_response["mutation_count"] / df_response["lineage_count"]
+        )
+        df_response.loc[~df_response["codon_end"].isna(), "change_length_nt"] = (
+            (df_response["codon_end"] - df_response["codon_num"]) + 1
+        ) * 3
+        df_response = df_response[df_response["prevalence"] >= frequency].fillna("None")
+        if genes:
+            df_response = df_response[df_response["gene"].str.lower().isin(genes)]
+        dict_response[lineage["key"]] = df_response.to_dict(orient="records")
+
     return dict_response
