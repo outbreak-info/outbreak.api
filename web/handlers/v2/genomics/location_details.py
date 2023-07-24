b4a84b64b4fd606af1e77e3b51e832964c97f351
everaldorodrigo <everaldorodrigo@gmail.com>
2023-07-24 12:31:40 -0700

    Refactor location-details.

--- web/handlers/v2/genomics/location_details.py
+++ web/handlers/v2/genomics/location_details.py
@@ -1,5 +1,6 @@
+import web.handlers.v2.genomics.helpers.location_details as helper
+
 from web.handlers.genomics.base import BaseHandler
-from web.handlers.genomics.util import parse_location_id_to_query
 
 
 class LocationDetailsHandler(BaseHandler):
@@ -12,72 +13,8 @@ class LocationDetailsHandler(BaseHandler):
     async def _get(self):
         query_str = self.args.id
         query_ids = query_str.split("_")
-        query = {"query": {}, "aggs": {"loc": {"composite": {"size": 10000, "sources": []}}}}
-        loc_id_len = len(query_ids)
-        if loc_id_len >= 1:
-            query["aggs"]["loc"]["composite"]["sources"].extend(
-                [
-                    {"country": {"terms": {"field": "country"}}},
-                    {"country_id": {"terms": {"field": "country_id"}}},
-                ]
-            )
-        if loc_id_len >= 2:  # 3 is max length
-            query["aggs"]["loc"]["composite"]["sources"].extend(
-                [
-                    {"division": {"terms": {"field": "division"}}},
-                    {"division_id": {"terms": {"field": "division_id"}}},
-                ]
-            )
-        if loc_id_len == 3:  # 3 is max length
-            query["aggs"]["loc"]["composite"]["sources"].extend(
-                [
-                    {"location": {"terms": {"field": "location"}}},
-                    {"location_id": {"terms": {"field": "location_id"}}},
-                ]
-            )
-        query["query"] = parse_location_id_to_query(query_str)
-        self.observability.log("es_query_before", query)
-        resp = await self.asynchronous_fetch(query)
-        self.observability.log("es_query_after", query)
-        flattened_response = []
-        for rec in resp["aggregations"]["loc"]["buckets"]:
-            if loc_id_len == 1:
-                flattened_response.append(
-                    {
-                        "country": rec["key"]["country"],
-                        "country_id": rec["key"]["country_id"],
-                        "label": rec["key"]["country"],
-                        "admin_level": 0,
-                    }
-                )
-            elif loc_id_len == 2:
-                flattened_response.append(
-                    {
-                        "division": rec["key"]["division"],
-                        "division_id": rec["key"]["division_id"],
-                        "country": rec["key"]["country"],
-                        "country_id": rec["key"]["country_id"],
-                        "label": ", ".join([rec["key"]["division"], rec["key"]["country"]]),
-                        "admin_level": 1,
-                    }
-                )
-            elif loc_id_len == 3:
-                flattened_response.append(
-                    {
-                        "location": rec["key"]["location"],
-                        "location_id": rec["key"]["location_id"],
-                        "division": rec["key"]["division"],
-                        "division_id": rec["key"]["division_id"],
-                        "country": rec["key"]["country"],
-                        "country_id": rec["key"]["country_id"],
-                        "label": ", ".join(
-                            [rec["key"]["location"], rec["key"]["division"], rec["key"]["country"]]
-                        ),
-                        "admin_level": 2,
-                    }
-                )
-        if len(flattened_response) >= 1:
-            flattened_response = flattened_response[0]  # ID should match only 1 region
-        flattened_response["query_id"] = query_str
-        resp = {"success": True, "results": flattened_response}
+        query = helper.create_query(query_ids=query_ids, query_str=query_str)
+        query_resp = await self.asynchronous_fetch(query)
+        parsed_resp = helper.parse_response(query_resp=query_resp, query_ids=query_ids, query_str=query_str)
+        resp = {"success": True, "results": parsed_resp}
         return resp
