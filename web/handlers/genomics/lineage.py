07b637cd00b0d351490bbb4e42139e8718eb8ce6
everaldorodrigo <everaldorodrigo@gmail.com>
2023-07-25 17:21:24 -0700

    Refactor /mutations-by-lineage.

--- web/handlers/genomics/lineage.py
+++ web/handlers/genomics/lineage.py
@@ -198,7 +198,6 @@ class LineageMutationsHandler(BaseHandler):
             if len(query_lineage_split) > 1:
                 query_mutations = query_lineage_split[1:] # First parameter is always lineage
             query["query"] = create_nested_mutation_query(lineages = query_pangolin_lineage, mutations = query_mutations)
-            #print(query)
             resp = yield self.asynchronous_fetch(query)
             path_to_results = ["aggregations", "mutations", "mutations", "buckets"]
             buckets = resp
@@ -282,20 +281,21 @@ class MutationsByLineage(BaseHandler):
         query_location = self.get_argument("location_id", None)
         query_mutations = self.get_argument("mutations", None)
         query_pangolin_lineage = self.get_argument("pangolin_lineage", None)
-        query_mutations = [muts.split(",") for muts in query_mutations.split(" AND ")] if query_mutations is not None else []
+        query_mutations = [muts for muts in query_mutations.split(" AND ")] if query_mutations is not None else []
         query_frequency_threshold = self.get_argument("frequency", None)
         query_frequency_threshold = float(query_frequency_threshold) if query_frequency_threshold is not None else 0
         results = {}
+
         for muts in query_mutations: # For multiple sets of mutations, create multiple ES queries. Since AND queries are possible doing one ES query with aggregations is cumbersome. Must look for better solution here.
             query = {
                 "size": 0,
                 "aggs": {
-	            "lineage": {
-                        "terms": {"field": "pangolin_lineage", "size": self.size},
+                "lineage": {
+                        "terms": {"field": "pangolin_lineage.keyword", "size": self.size},
                         "aggs": {
                             "mutations": {
                                 "filter": {}
-			    }
+                            }
                         }
                     }
                 }
@@ -306,13 +306,13 @@ class MutationsByLineage(BaseHandler):
                 if "query" in query: # Only query added will be bool for location
                     query["query"]["bool"]["must"].append({
                         "term": {
-                            "pangolin_lineage": query_pangolin_lineage
+                            "pangolin_lineage.keyword": query_pangolin_lineage
                         }
                     })
                 else:
                     query["query"] = {
                         "term": {
-                            "pangolin_lineage": query_pangolin_lineage
+                            "pangolin_lineage.keyword": query_pangolin_lineage
                         }
                     }
             query["aggs"]["lineage"]["aggs"]["mutations"]["filter"] = create_nested_mutation_query(mutations = muts)
@@ -337,6 +337,7 @@ class MutationsByLineage(BaseHandler):
                 df_response.loc[:, "proportion_ci_lower"] = prop[1]
                 df_response.loc[:, "proportion_ci_upper"] = prop[2]
             df_response = df_response[df_response["proportion"] >= query_frequency_threshold]
-            results[",".join(muts)] = df_response.to_dict(orient="records")
+            results[muts] = df_response.to_dict(orient="records")
+
         resp = {"success": True, "results": results}
         return resp
