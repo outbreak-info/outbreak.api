80b089aa744ffc02f5ffb71102ee62866a4d6dda
everaldorodrigo <everaldorodrigo@gmail.com>
2023-08-28 13:31:14 -0700

    Refactor ./global-prevalence on v3.

---
+++ web/handlers/v3/genomics/helpers/prevalence_helper.py
@@ -0,0 +1,56 @@
+def params_adapter(args):
+    params = {}
+    params["pangolin_lineage"] = args.pangolin_lineage or None
+    params["mutations"] = args.mutations or None
+    params["cumulative"] = args.cumulative
+    return params
+
+def create_query_filter(params):
+    lineages = params["pangolin_lineage"]
+    mutations = params["mutations"]
+    filters = []
+
+    if lineages is None and mutations is None:
+        return "*"
+
+    if lineages is not None and len(lineages) > 0:
+        lineages = params["pangolin_lineage"]
+        lineages = "pangolin_lineage: ({})".format(lineages)
+        filters.append(lineages)
+    if mutations is not None and len(mutations) > 0:
+        mutations = params["mutations"]
+        mutations = mutations.replace(":","\\:")
+        mutations = "mutations: ({})".format(mutations)
+        filters.append(mutations)
+    query_filters = " AND ".join(filters)
+    return query_filters
+
+def create_query(params, size):
+    query_filters = create_query_filter(params)
+    query = {
+        "size": 0,
+        "aggs": {
+            "prevalence": {
+                "terms": {
+                    "field": "date_collected",
+                    "size": size
+                },
+                "aggs": {
+                    "lineage_count": {
+                        "filter": {
+                            "bool": {
+                                "must": [
+                                        {
+                                        "query_string": {
+                                            "query": query_filters # Ex: "(pangolin_lineage:BA.2) AND (mutations: S\\:E484K OR S\\:L18F)"
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return query
--- web/handlers/v3/genomics/prevalence.py
+++ web/handlers/v3/genomics/prevalence.py
@@ -1,40 +1,25 @@
 from .util import (
-    transform_prevalence,
-    create_nested_mutation_query
+    transform_prevalence
 )
+import web.handlers.v3.genomics.helpers.prevalence_helper as helper
 from web.handlers.v3.genomics.base import BaseHandlerV3
-from tornado import gen
 
 # Get global prevalence of lineage by date
 class GlobalPrevalenceByTimeHandler(BaseHandlerV3):
+    name = "global-prevalence-by-time"
+    kwargs = dict(BaseHandlerV3.kwargs)
+    kwargs["GET"] = {
+        "pangolin_lineage": {"type": str, "required": False},
+        "mutations": {"type": str, "required": False},
+        "cumulative": {"type": bool, "required": False}
+    }
 
-    @gen.coroutine
-    def _get(self):
-        query_pangolin_lineage = self.get_argument("pangolin_lineage", None)
-        query_mutations = self.get_argument("mutations", None)
-        cumulative = self.get_argument("cumulative", None)
-        cumulative = True if cumulative == "true" else False
-        query = {
-            "size": 0,
-            "aggs": {
-                "prevalence": {
-                    "terms": {
-                        "field": "date_collected",
-                        "size": self.size
-                    },
-                    "aggs": {
-                        "lineage_count": {
-                            "filter": {}
-                        }
-                    }
-                }
-            }
-        }
-        query_obj = create_nested_mutation_query(lineages = query_pangolin_lineage, mutations = query_mutations)
-        query["aggs"]["prevalence"]["aggs"]["lineage_count"]["filter"] = query_obj
-        resp = yield self.asynchronous_fetch(query)
+    async def _get(self):
+        params = helper.params_adapter(self.args)
+        query = helper.create_query(params, self.size)
+        query_resp = await self.asynchronous_fetch(query)
         path_to_results = ["aggregations", "prevalence", "buckets"]
-        resp = transform_prevalence(resp, path_to_results, cumulative)
+        resp = transform_prevalence(query_resp, path_to_results, params["cumulative"])
         return {
             "success": True,
             "results": resp
