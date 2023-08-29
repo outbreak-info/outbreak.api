a8888e59c29abd6b36051ea44dd6c56128f78d5c
everaldorodrigo <everaldorodrigo@gmail.com>
2023-08-29 16:18:09 -0700

    Refactor ./mutations-by-lineage

---
+++ web/handlers/v3/genomics/helpers/mutations_by_lineage_helper.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+from typing import Dict
+from web.handlers.v3.genomics.util import (
+    create_nested_mutation_query,
+    parse_location_id_to_query,
+    calculate_proportion,
+)
+
+
+def params_adapter(args):
+    params = {}
+    params["pangolin_lineage"] = args.pangolin_lineage or None
+    params["mutations"] = args.mutations or None
+    params["location_id"] = args.location_id or None
+    params["frequency"] = args.frequency
+    return params
+
+def create_query(params, size):
+    query = {
+        "size": 0,
+        "aggs": {
+        "lineage": {
+                "terms": {"field": "pangolin_lineage", "size": size},
+                "aggs": {
+                    "mutations": {
+                        "filter": {}
+                    }
+                }
+            }
+        }
+    }
+    if params["location_id"] is not None:
+        query["query"] = parse_location_id_to_query(params["location_id"])
+    if params["pangolin_lineage"] is not None:
+        if "query" in query: # Only query added will be bool for location
+            query["query"]["bool"]["must"].append({
+                "term": {
+                    "pangolin_lineage": params["pangolin_lineage"]
+                }
+            })
+        else:
+            query["query"] = {
+                "term": {
+                    "pangolin_lineage": params["pangolin_lineage"]
+                }
+            }
+    query["aggs"]["lineage"]["aggs"]["mutations"]["filter"] = create_nested_mutation_query(mutations = params["mutations"])
+    return query
+
+def parse_response(resp: Dict = None, params: Dict = None):
+    results = {}
+    path_to_results = ["aggregations", "lineage", "buckets"]
+    buckets = resp
+    for i in path_to_results:
+        buckets = buckets[i]
+    flattened_response = []
+    for i in buckets:
+        if not i["mutations"]["doc_count"] > 0 or i["key"] == "none":
+            continue
+        flattened_response.append({
+            "pangolin_lineage": i["key"],
+            "lineage_count": i["doc_count"],
+            "mutation_count": i["mutations"]["doc_count"]
+        })
+    df_response = pd.DataFrame(flattened_response)
+    if df_response.shape[0] > 0:
+        prop = calculate_proportion(df_response["mutation_count"], df_response["lineage_count"])
+        df_response.loc[:, "proportion"] = prop[0]
+        df_response.loc[:, "proportion_ci_lower"] = prop[1]
+        df_response.loc[:, "proportion_ci_upper"] = prop[2]
+    df_response = df_response[df_response["proportion"] >= params["frequency"]]
+    results[params["mutations"]] = df_response.to_dict(orient="records")
+    return results
