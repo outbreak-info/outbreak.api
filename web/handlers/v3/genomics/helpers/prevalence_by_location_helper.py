add315a6d299eec86e2845354338bf637b6f5089
everaldorodrigo <everaldorodrigo@gmail.com>
2023-08-29 10:07:02 -0700

    Refactor ./prevalence-by-location

---
+++ web/handlers/v3/genomics/helpers/prevalence_by_location_helper.py
@@ -0,0 +1,79 @@
+from typing import Dict
+from web.handlers.v3.genomics.util import (
+    create_nested_mutation_query,
+    create_query_filter,
+    parse_location_id_to_query,
+    transform_prevalence,
+)
+
+def params_adapter(args):
+    params = {}
+    params["pangolin_lineage"] = args.pangolin_lineage or None
+    params["mutations"] = args.mutations or None
+    params["location_id"] = args.location_id or None
+    params["cumulative"] = args.cumulative
+    params["min_date"] = args.min_date or None
+    params["max_date"] = args.max_date or None
+    return params
+
+def create_query(params, size):
+    query = {
+        "size": 0,
+        "aggs": {
+            "prevalence": {
+                "filter": {"bool": {"must": []}},
+                "aggs": {
+                    "count": {
+                        "terms": {"field": "date_collected", "size": size},
+                        "aggs": {"lineage_count": {"filter": {}}},
+                    }
+                },
+            }
+        },
+    }
+
+    date_range_filter = {"query": {"range": {"date_collected": {}}}}
+    if params["max_date"]:
+        date_range_filter["query"]["range"]["date_collected"]["lte"] = params["max_date"]
+    if params["min_date"]:
+        date_range_filter["query"]["range"]["date_collected"]["gte"] = params["min_date"]
+    if params["max_date"] or params["min_date"]:
+        query.update(date_range_filter)
+
+    parse_location_id_to_query(params["location_id"], query["aggs"]["prevalence"]["filter"])
+
+    lineages = params["pangolin_lineage"] if params["pangolin_lineage"] is not None else ""
+    mutations = params["mutations"] if params["mutations"] is not None else ""
+    query_obj = create_nested_mutation_query(
+        lineages=lineages, mutations=mutations, location_id=params["location_id"]
+    )
+    query["aggs"]["prevalence"]["aggs"]["count"]["aggs"]["lineage_count"][
+        "filter"
+    ] = query_obj
+
+    return query
+
+def parse_response(resp: Dict = None, params: Dict = None):
+    results = {}
+    path_to_results = ["aggregations", "prevalence", "count", "buckets"]
+    resp = transform_prevalence(resp, path_to_results, params["cumulative"])
+    lineages = params["pangolin_lineage"] if params["pangolin_lineage"] is not None else ""
+    mutations = params["mutations"] if params["mutations"] is not None else ""
+    # TODO: What should be returned as `res_key`?
+    # res_key = None
+    # if len(query_pangolin_lineage) > 0:
+    #     res_key = " OR ".join(lineages)
+    # if len(query_mutations) > 0:
+    #     res_key = (
+    #         "({}) AND ({})".format(res_key, " AND ".join(query_mutations))
+    #         if res_key is not None
+    #         else " AND ".join(query_mutations)
+    #     )
+    # TODO: Trying to keep a similar behavior for `res_key` for now.
+    res_key = create_query_filter(lineages=lineages, mutations=mutations, locations=params["location_id"])
+    res_key = res_key.replace("pangolin_lineage: ", "")
+    res_key = res_key.replace("mutations: ", "")
+    res_key = res_key.replace("country_id: ", "")
+    res_key = res_key.replace("\\", "")
+    results[res_key] = resp
+    return results
--- web/handlers/v3/genomics/prevalence_by_location_and_time.py
+++ web/handlers/v3/genomics/prevalence_by_location_and_time.py
@@ -1,10 +1,6 @@
+import web.handlers.v3.genomics.helpers.prevalence_by_location_helper as helper
+
 from web.handlers.v3.genomics.base import BaseHandlerV3
-from web.handlers.v3.genomics.util import (
-    create_nested_mutation_query,
-    create_query_filter,
-    parse_location_id_to_query,
-    transform_prevalence,
-)
 
 
 class PrevalenceByLocationAndTimeHandler(BaseHandlerV3):
@@ -20,67 +16,9 @@ class PrevalenceByLocationAndTimeHandler(BaseHandlerV3):
     }
 
     async def _get(self):
-        query_location = self.args.location_id
-        query_pangolin_lineage = self.args.pangolin_lineage
-        # query_pangolin_lineage = (
-        #     query_pangolin_lineage.split(",") if query_pangolin_lineage is not None else []
-        # )
-        query_mutations = self.args.mutations
-        # query_mutations = query_mutations.split(" AND ") if query_mutations is not None else []
-        cumulative = self.args.cumulative
-        date_range_filter = {"query": {"range": {"date_collected": {}}}}
-        if self.args.max_date:
-            date_range_filter["query"]["range"]["date_collected"]["lte"] = self.args.max_date
-        if self.args.min_date:
-            date_range_filter["query"]["range"]["date_collected"]["gte"] = self.args.min_date
-
-        results = {}
-        # for i, j in create_iterator(query_pangolin_lineage, query_mutations):
-        query = {
-            "size": 0,
-            "aggs": {
-                "prevalence": {
-                    "filter": {"bool": {"must": []}},
-                    "aggs": {
-                        "count": {
-                            "terms": {"field": "date_collected", "size": self.size},
-                            "aggs": {"lineage_count": {"filter": {}}},
-                        }
-                    },
-                }
-            },
-        }
-        if self.args.max_date or self.args.min_date:
-            query.update(date_range_filter)
-        parse_location_id_to_query(query_location, query["aggs"]["prevalence"]["filter"])
-        # lineages = i.split(" OR ") if i is not None else []
-        lineages = query_pangolin_lineage if query_pangolin_lineage is not None else ""
-        mutations = query_mutations if query_mutations is not None else ""
-        query_obj = create_nested_mutation_query(
-            lineages=lineages, mutations=mutations, location_id=query_location
-        )
-        query["aggs"]["prevalence"]["aggs"]["count"]["aggs"]["lineage_count"][
-            "filter"
-        ] = query_obj
-        self.observability.log("es_query_before", query)
-        resp = await self.asynchronous_fetch(query)
-        path_to_results = ["aggregations", "prevalence", "count", "buckets"]
-        resp = transform_prevalence(resp, path_to_results, cumulative)
-        # TODO: What should be returned as `res_key`?
-        # res_key = None
-        # if len(query_pangolin_lineage) > 0:
-        #     res_key = " OR ".join(lineages)
-        # if len(query_mutations) > 0:
-        #     res_key = (
-        #         "({}) AND ({})".format(res_key, " AND ".join(query_mutations))
-        #         if res_key is not None
-        #         else " AND ".join(query_mutations)
-        #     )
-        # TODO: Trying to keep a similar behavior for `res_key` for now.
-        res_key = create_query_filter(lineages=lineages, mutations=mutations, locations=query_location)
-        res_key = res_key.replace("pangolin_lineage: ", "")
-        res_key = res_key.replace("mutations: ", "")
-        res_key = res_key.replace("country_id: ", "")
-        res_key = res_key.replace("\\", "")
-        results[res_key] = resp
-        return {"success": True, "results": results}
+        params = helper.params_adapter(self.args)
+        query = helper.create_query(params, self.size)
+        query_resp = await self.asynchronous_fetch(query)
+        parsed_resp = helper.parse_response(query_resp, params)
+        resp = {"success": True, "results": parsed_resp}
+        return resp
