c827110320f5d611eb94ee876cf9d36fd7e7098e
everaldorodrigo <everaldorodrigo@gmail.com>
2023-09-14 09:42:16 -0700

    Add 'q' param to lineage-mutations v3.

--- web/handlers/v3/genomics/helpers/lineage_mutations_helper.py
+++ web/handlers/v3/genomics/helpers/lineage_mutations_helper.py
@@ -85,6 +85,25 @@ def create_query(lineages: str = "", mutations: str = "") -> Dict:
     return query
 
 
+def create_query_q(query_filters: str = "", size: int = 10000) -> Dict:
+    query = {
+        "size": 0,
+        "track_total_hits": True,
+        "query": {
+            "bool": {
+                "filter": [
+                    {
+                        "query_string": {
+                            "query": query_filters  # Ex: (pangolin_lineage : BA.2) AND (mutations : NOT(MUTATION) OR MUTATION)
+                        }
+                    }
+                ]
+            }
+        },
+        "aggs": {"mutations": {"terms": {"field": "mutations", "size": size}}},
+    }
+    return query
+
 def parse_response(
     resp: Dict = None, frequency: int = 1, lineages: str = "", genes: Optional[List[str]] = None
 ) -> Dict:
@@ -93,6 +112,81 @@ def parse_response(
     lineage = resp["aggregations"]
     if len(lineages) > 0:
         lineage["key"] = lineages
+
+    path_to_results = ["mutations", "buckets"]
+
+    buckets = lineage
+    for i in path_to_results:
+        buckets = buckets[i]
+    flattened_response = [
+        {
+            "mutation": i["key"],
+            "mutation_count": i["doc_count"],
+            "lineage_count": resp["hits"]["total"]["value"],
+            "lineage": lineage["key"],
+        }
+        for i in buckets
+    ]
+
+    if len(flattened_response) > 0:
+        df_response = pd.DataFrame(flattened_response).assign(
+            gene=lambda x: x["mutation"].apply(
+                lambda k: gene_mapping()[k.split(":")[0]]
+                if k.split(":")[0] in gene_mapping()
+                else k.split(":")[0]
+            ),
+            ref_aa=lambda x: x["mutation"]
+            .apply(
+                lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[0]
+                if "DEL" not in k and "del" not in k and "_" not in k
+                else k
+            )
+            .str.upper(),
+            alt_aa=lambda x: x["mutation"]
+            .apply(
+                lambda k: re.findall("[A-Za-z*]+", k.split(":")[1])[1]
+                if "DEL" not in k and "del" not in k and "_" not in k
+                else k.split(":")[1]
+            )
+            .str.upper(),
+            codon_num=lambda x: x["mutation"].apply(
+                lambda k: int(re.findall("[0-9]+", k.split(":")[1])[0])
+            ),
+            codon_end=lambda x: x["mutation"].apply(
+                lambda k: int(re.findall("[0-9]+", k.split(":")[1])[1])
+                if "/" in k and ("DEL" in k or "del" in k)
+                else None
+            ),
+            type=lambda x: x["mutation"].apply(
+                lambda k: "deletion" if "DEL" in k or "del" in k else "substitution"
+            ),
+        )
+        df_response = df_response[df_response["ref_aa"] != df_response["alt_aa"]]
+        df_response.loc[:, "prevalence"] = (
+            df_response["mutation_count"] / df_response["lineage_count"]
+        )
+        df_response.loc[~df_response["codon_end"].isna(), "change_length_nt"] = (
+            (df_response["codon_end"] - df_response["codon_num"]) + 1
+        ) * 3
+        df_response = (
+            df_response[df_response["prevalence"] >= frequency]
+            .fillna(pd.NA)
+            .replace([pd.NA], [None])
+        )
+        if genes:
+            df_response = df_response[df_response["gene"].str.lower().isin(genes)]
+        dict_response[lineage["key"]] = df_response.to_dict(orient="records")
+    return dict_response
+
+
+def parse_response_q(resp: Dict = None, idx: int = 0, params: Dict = None) -> Dict:
+    dict_response = {}
+
+    lineage = resp["aggregations"]
+    lineage["key"] = params["q_list"][idx]
+
+    frequency = params["frequency"]
+    genes = params["genes"]
 
     path_to_results = ["mutations", "buckets"]
 
--- web/handlers/v3/genomics/lineage_mutations.py
+++ web/handlers/v3/genomics/lineage_mutations.py
@@ -1,11 +1,15 @@
+import asyncio
+
 import web.handlers.v3.genomics.helpers.lineage_mutations_helper as helper
 from web.handlers.v3.genomics.base import BaseHandlerV3
+from web.handlers.v3.genomics.util import create_iterator_q
 
 
 class LineageMutationsHandler(BaseHandlerV3):
     name = "lineage-mutations-v3"
     kwargs = dict(BaseHandlerV3.kwargs)
     kwargs["GET"] = {
+        "q": {"type": str, "default": None},
         "pangolin_lineage": {"type": str},
         "lineages": {"type": str},
         "mutations": {"type": str},
@@ -15,12 +19,26 @@ class LineageMutationsHandler(BaseHandlerV3):
 
     async def _get(self):
         params = helper.params_adapter(self.args)
-        query = helper.create_query(lineages=params["lineages"], mutations=params["mutations"])
-        self.observability.log("es_query_before", query)
-        resp = await self.asynchronous_fetch(query=query)
-        self.observability.log("es_query_after", query)
-        dict_response = helper.parse_response(
-            resp=resp, frequency=params["frequency"], lineages=params["lineages"], genes=params["genes"]
-        )
-        result = {"success": True, "results": dict_response}
+
+        parsed_resp= {}
+        if (params["lineages"] is not None and params["lineages"] != "") or (params["mutations"] is not None and params["mutations"] != ""):
+            query = helper.create_query(lineages=params["lineages"], mutations=params["mutations"])
+            resp = await self.asynchronous_fetch(query=query)
+            parsed_resp = helper.parse_response(
+                resp=resp, frequency=params["frequency"], lineages=params["lineages"], genes=params["genes"]
+            )
+
+        if "q" in params and params["q"] is not None:
+
+            async def process_query_q(idx, query_filter):
+                query = helper.create_query_q(query_filter, self.size)
+                self.observability.log("ES_QUERY", query)
+                query_resp = await self.asynchronous_fetch(query)
+                # self.observability.log("ES_RESPONSE", query_resp)
+                parsed_resp.update(helper.parse_response_q(resp=query_resp, idx=idx, params=params))
+
+            tasks = [process_query_q(idx, query_filter) for idx, query_filter in create_iterator_q(params["query_string_list"])]
+            await asyncio.gather(*tasks)
+
+        result = {"success": True, "results": parsed_resp}
         return result
