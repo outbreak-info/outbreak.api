5768b715bf5c637e2209acc55140dd428cbe39be
Everaldo <everaldorodrigo@gmail.com>
2024-03-21 12:11:00 -0700

    Add old wastewater api handler.

--- config_web/__init__.py
+++ config_web/__init__.py
@@ -1,4 +1,4 @@
-from . import covid19, genomics, growth_rate, resources, significance, wastewater_demix, wastewater_metadata, wastewater_variants
+from . import covid19, genomics, growth_rate, resources, significance, wastewater, wastewater_demix, wastewater_metadata, wastewater_variants
 
 try:
     from config_web_local import *
