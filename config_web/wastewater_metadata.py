a4fe921c75edc95efd464a5529e5eed0b5b24fb6
Everaldo <everaldorodrigo@gmail.com>
2025-01-27 10:10:13 -0800

    Add wastewater endpoints. (#107)
    * Add wastewater endpoints.
    
    * Create endpoints for demix, metadata, and variants.
    
    * Create endpoints for demix, metadata, and variants.
    
    * Create endpoints for demix, metadata, and variants.
    
    * Add old wastewater api handler.
    
    * Remove authentication from wastewater endpoints.
    
    * Add Wastewater demix weekly endpoint.

---
+++ config_web/wastewater_metadata.py
@@ -0,0 +1,12 @@
+# *****************************************************************************
+# Elasticsearch variables
+# *****************************************************************************
+ES_INDEX = "outbreak-wastewater-metadata"
+API_PREFIX = "wastewater_metadata"
+ES_DOC_TYPE = "wastewater_metadata"
+API_VERSION = ""
+
+APP_LIST = [
+    (r"/{pre}/{ver}/metadata/?", "web.handlers.WWMetadataSourceHandler"),
+    (r"/{pre}/{ver}/query/?", "web.handlers.WWQueryHandler"),
+]
